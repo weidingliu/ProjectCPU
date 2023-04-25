c6a4d463df5cc8625001975bb8fc1cb331565ac5
liuweidin <1045251744@qq.com>
2023-04-25 16:25:58 +0800

    trm and break

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ distributed shark hands five-stage CPU base on LA32r
 
 How to use?
 cpu-tests is programer, you can program yourself test script to test you design,and I use C++ to build the testbench with difftest.
+
+Because we only implement a simple abstract machine, this testbench can't stop by itself.
--- cpu-tests/trm.c
+++ cpu-tests/trm.c
@@ -1,9 +1,19 @@
 
 //static const char mainargs[] = MAINARGS;
 
+# define nemu_trap(code) asm volatile("or $s0,$zero,%0; break 1" : :"r"(code))
+
+void halt(int code) {
+  nemu_trap(code);
+
+  // should not reach here
+  while (1);
+}
+
+
 void _trm_init() {
  // int ret = main(mainargs);
  int ret = main();
   //printf("is here\n");
-  //halt(ret);
+  halt(ret);
 }
