26c57ea766c9b45e1889910db5da8ff31a90aedc
liuweidin <1045251744@qq.com>
2023-05-09 12:45:38 +0800

    div and some doc

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-distributed shark hands five-stage CPU base on LA32r
+**distributed shark hands five-stage CPU base on LA32r**
 
 How to use?
 cpu-tests is programer, you can program yourself test script to test you design,and I use C++ to build the testbench with difftest.
+
+要运行单元测试
 
--- core/vsrc/backend/EXE.v
+++ core/vsrc/backend/EXE.v
@@ -53,17 +53,20 @@ wire bypass_en1;
 wire bypass_en2;
 wire [31:0] bypass_reg1;
 wire [31:0] bypass_reg2;
-wire [64:0]mul_result;
-wire [31:0]mod_result;
+// wire [64:0]mul_result;
+// wire [31:0]mod_result;
 wire [31:0]mul_div_result;
 wire is_mul_div;
-wire [31:0]div_result;
+// wire [31:0]div_result;
 wire is_mul;
 wire is_div;
 wire [31:0] mul_hi;
 wire [31:0]mul_lo;
 wire mul_valid;
 wire logic_valid;
+wire div_valid;
+wire [31:0]quotient;
+wire [31:0]remainder;
 
 
 wire branch_flag;
@@ -126,18 +129,18 @@ assign is_mul_div = mul_div_op[0] | mul_div_op[1] | mul_div_op[2] | mul_div_op[3
 
 // assign mul_result = ({64{is_sign}} & ($signed(src1)*$signed(src2))) | 
 //                     ({64{~is_sign}} & (src1*src2));
-assign mod_result = ({32{is_sign}} & ($signed(src1) % $signed(src2)) |
-                    ({32{~is_sign}} & (src1 % src2))
-                    );
-assign div_result = (
-    ({32{is_sign}} & ($signed(src1) / $signed(src2))) | 
-    ({32{~is_sign}} & (src1 / src2))
-);
+// assign mod_result = ({32{is_sign}} & ($signed(src1) % $signed(src2)) |
+//                     ({32{~is_sign}} & (src1 % src2))
+//                     );
+// assign div_result = (
+//     ({32{is_sign}} & ($signed(src1) / $signed(src2))) | 
+//     ({32{~is_sign}} & (src1 / src2))
+// );
 
 assign mul_div_result = ({32{mul_div_op[0]}} & mul_lo)|
-                        ({32{mul_div_op[1]}} & mod_result)|
+                        ({32{mul_div_op[1]}} & remainder)|
                         ({32{mul_div_op[2]}} & mul_hi)|
-                        ({32{mul_div_op[3]}} & div_result); 
+                        ({32{mul_div_op[3]}} & quotient); 
 assign is_mul = mul_div_op[0] | mul_div_op[1] | mul_div_op[2];
 assign is_div = mul_div_op[3];
 // booth multiplier
@@ -155,6 +158,21 @@ Booth_MUL MUL(
     .io_out_valid(mul_valid),//is mul end
     .io_out_bits_result_result_hi(mul_hi),//hight 32-bit
     .io_out_bits_result_result_lo(mul_lo)// low 32-bit
+);
+DIV DIV(
+    .clock(clk),
+    .reset(reset),
+    .io_in_ready(),
+    .io_in_valid(is_div),
+    .io_in_bits_ctrl_flow_divw(),
+    .io_in_bits_ctrl_flow_div_signed(is_sign),
+    .io_in_bits_ctrl_flow_flush(1'b0),
+    .io_in_bits_ctrl_data_src1(src1),
+    .io_in_bits_ctrl_data_src2(src2),
+    .io_out_ready(1'b1),
+    .io_out_valid(div_valid),
+    .io_out_bits_result_quotient(quotient),
+    .io_out_bits_result_remainder(remainder)
 );
 
 
@@ -203,8 +221,8 @@ always @(posedge clk) begin
 end
 // output logic
 assign right_valid=valid;
-assign logic_valid = (!mul_valid && is_mul)? 1'b0:1'b1;
-assign left_ready=(!mul_valid && is_mul)? 1'b0:right_ready;
+assign logic_valid = (!mul_valid && is_mul || !div_valid && is_div)? 1'b0:1'b1;
+assign left_ready=(!mul_valid && is_mul || !div_valid && is_div )? 1'b0:right_ready;
 assign ex_ctrl_bus=ctrl_temp_bus;
 assign ex_bypass = {alu_result,wreg_index,wreg_en};
 
