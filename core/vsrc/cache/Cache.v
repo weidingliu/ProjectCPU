45b19eb194e2e71f40891014f6570756f214e217
liuweidin <1045251744@qq.com>
2023-05-31 20:37:16 +0800

    cache and sram like

--- core/vsrc/cache/Cache.v
+++ core/vsrc/cache/Cache.v
@@ -5,17 +5,37 @@
 module Cache_Data #(
    parameter BUS_WIDTH = 32,
    parameter DATA_WIDTH = 32,
-   parameter Cache_line_size = DATA_WIDTH * 16, //64B
-   parameter Cache_line_wordnum = Cache_line_size / DATA_WIDTH,
-   parameter Cache_way = 2,
+   parameter Cache_line_wordnum = 16,
    parameter Max_size = 4096,//4kB
-   parameter Cache_line_num = Max_size / Cache_way / Cache_line_size * 8, //4kB
-   parameter Tag_size = BUS_WIDTH - $clog2(Cache_line_size/8) - $clog2(Cache_line_num)
+
+   localparam Cache_line_size = DATA_WIDTH * Cache_line_wordnum, //64B
+   localparam Cache_way = 2,
+   localparam Cache_line_num = Max_size / Cache_way / Cache_line_size * 8, //4kB
+   localparam Tag_size = BUS_WIDTH - $clog2(Cache_line_size/8) - $clog2(Cache_line_num),
+   localparam Index_size =  $clog2(Cache_line_num),
+   localparam Offset_size = $clog2(Cache_line_size/8)
 ) (
-    input wire [BUS_WIDTH-1:0] addr
-    // input wire 
+    input wire clk,
+    input wire reset,
+    input wire [BUS_WIDTH-1:0] addr,
+
+    output wire  [Cache_line_size-1:0] way0_data,
+    output wire  [Cache_line_size-1:0] way1_data,
+
+    output wire [Tag_size-1:0] way0_tag,
+    output wire [Tag_size-1:0] way1_tag,
+
+    output wire [Index_size-1:0]index,
+    output wire [Offset_size-1:0]offset,
+    output wire [Tag_size-1:0] tag
 );
-    
+
+
+
+assign offset = addr[Offset_size-1:0];
+assign index = addr[Index_size + Offset_size-1:Offset_size];
+assign tag = addr[BUS_WIDTH-1:Index_size + Offset_size];
+
 endmodule
 
 /*
@@ -31,13 +51,13 @@ endmodule //Cache
 module DCache #(
    parameter BUS_WIDTH = 32,
    parameter DATA_WIDTH = 32,
-   parameter Cache_line_size = DATA_WIDTH * 16, //64B
-   parameter Cache_line_wordnum = Cache_line_size / DATA_WIDTH,
-   parameter Cache_way = 2,
+   parameter Cache_line_wordnum = 16,
    parameter Max_size = 4096,//4kB
-   parameter Cache_line_num = Max_size / Cache_way / Cache_line_size * 8, //4kB
-   parameter Tag_size = BUS_WIDTH - $clog2(Cache_line_size/8) - $clog2(Cache_line_num)
-
+   
+   localparam Cache_line_size = DATA_WIDTH * Cache_line_wordnum, //64B
+   localparam Cache_way = 2,
+   localparam Cache_line_num = Max_size / Cache_way / Cache_line_size * 8, //4kB
+   localparam Tag_size = BUS_WIDTH - $clog2(Cache_line_size/8) - $clog2(Cache_line_num)
 )(
     input wire clk,
     input wire reset,
@@ -80,13 +100,13 @@ endmodule //Cache
 module ICache #(
    parameter BUS_WIDTH = 32,
    parameter DATA_WIDTH = 32,
-   parameter Cache_line_size = DATA_WIDTH * 16, //64B
-   parameter Cache_line_wordnum = Cache_line_size / DATA_WIDTH,
-   parameter Cache_way = 2,
+   parameter Cache_line_wordnum = 16,
    parameter Max_size = 4096,//4kB
-   parameter Cache_line_num = Max_size / Cache_way / Cache_line_size * 8, //4kB
-   parameter Tag_size = BUS_WIDTH - $clog2(Cache_line_size/8) - $clog2(Cache_line_num)
-//    parameter DCACHE = 0
+   
+   localparam Cache_line_size = DATA_WIDTH * Cache_line_wordnum, //64B
+   localparam Cache_way = 2,
+   localparam Cache_line_num = Max_size / Cache_way / Cache_line_size * 8, //4kB
+   localparam Tag_size = BUS_WIDTH - $clog2(Cache_line_size/8) - $clog2(Cache_line_num)
 
 )(
     input wire clk,
@@ -130,21 +150,38 @@ reg [$clog2(Cache_line_wordnum)-1:0]read_count;
 endmodule //Cache
 
 /*
-* simulate sram , when clock rising edge read out data or write data
-*
+* simulate sram , when clock rising edge read out data or write data, signal port
+* write first memory
 */
 
 module Sramlike #(
     parameter DATA_WIDTH = 32,
-    parameter Addr_len = 6
-    
-    
+    parameter Addr_len = 6,
+    localparam Sram_Depth = $pow(2,Addr_len)
     //parameter $pow(2,)
 )(
     input wire clk,
-    input wire reset
+    input wire reset,
+
+    input wire [Addr_len-1:0] addr,
+    input wire [DATA_WIDTH-1:0] wdata,
+    input wire we,// 1'b0 is read, 1'b1 is write
+
+    output wire [Addr_len-1:0] rdata
 );
-localparameter Sram_Depth = $pow(2,Addr_len);
+
+reg [DATA_WIDTH-1:0]Mem[Sram_Depth-1:0];
+reg [Addr_len-1:0]addr_temp;
+
+always @(posedge clk) begin
+    if(we) begin 
+        Mem[addr] <= wdata;
+    end
+    addr_temp <= addr;
+end
+
+assign rdata = Mem[addr_temp];
+
     
 endmodule
 
