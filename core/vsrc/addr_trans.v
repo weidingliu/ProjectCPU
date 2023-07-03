6071a1ed10df2dceed5de6712f46e4e6563d6de0
liuweidin <1045251744@qq.com>
2023-07-03 22:58:28 +0800

    addr translate

--- core/vsrc/addr_trans.v
+++ core/vsrc/addr_trans.v
@@ -4,6 +4,10 @@
 module addr_trans (
     input wire clk,
     input wire reset,
+    //flush
+    input wire flush,
+    input wire excp_flush,
+    input wire ertn_flush,
     // from csr
     input wire [1:0]DAPG,
     input wire [31:0]DMW0,
@@ -12,14 +16,22 @@ module addr_trans (
     // inst interface
     input wire [31:0]inst_vaddr,
     input wire inst_addr_valid,
+    output wire inst_addr_ready,
+
     output wire inst_uncached_en,
     output wire [31:0]inst_paddr,
+    output wire inst_valid,
+    input wire inst_ready,
 
     //data addr interface
     input wire [31:0]data_vaddr,
     input wire data_addr_valid,
+    output wire data_addr_ready,
+
     output wire data_uncached_en,
-    output wire [31:0]data_paddr
+    output wire [31:0]data_paddr,
+    output wire data_valid,
+    input wire data_ready
 );
 
 
@@ -42,10 +54,18 @@ assign data_dmw0_en = (DMW0[31:29] == data_vaddr);
 assign data_dmw1_en = (DMW1[31:29] == data_vaddr);
 
 
-// vaddr
+// paddr
 assign inst_paddr = inst_trans_en? 32'h0:inst_vaddr;
 assign data_paddr = data_trans_en? 32'h0:data_vaddr;
 
+// 
+assign inst_uncached_en = ;
+assign data_uncached_en = ;
+
 
 endmodule //addr_trans
+
+
+//util
+
 
