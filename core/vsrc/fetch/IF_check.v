75810e302da6b05b19d8276b1dcd7e4ba65a4475
liuweidin <1045251744@qq.com>
2023-07-05 23:38:07 +0800

    addr_translate still coding

--- core/vsrc/fetch/IF_check.v
+++ core/vsrc/fetch/IF_check.v
@@ -19,13 +19,16 @@ module IF_check (
     output wire [31:0]vaddr_o,
     output wire ib_valid,
     input wire ib_ready
+    input wire fire,
+    output wire is_fire
 );
 reg valid;
 
 
 
 
-assign vaddr_ready = 
+
+assign vaddr_ready = (vaddr_valid & !inst_valid) ? 1'b0:1'b1;
 
 assign inst_ready = ib_ready;
 
